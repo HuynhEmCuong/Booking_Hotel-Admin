9b6a58fbe511fe166d0d091e20c261934cc8625b
EmCuongHuynh <huynhcuongem7597@gmail.com>
2022-06-13 22:39:12 +0700

    #add list room

--- src/app/core/services/room/room.service.ts
+++ src/app/core/services/room/room.service.ts
@@ -1,10 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Room } from '../../models/rooms/room/room';
+import { BaseService } from '../general/base.service';
+import { AuthService } from '../system/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RoomService {
-
-constructor() { }
-
+export class RoomService extends BaseService<Room> {
+  constructor(_auth: AuthService, _http: HttpClient, title: Title) {
+    super(_auth, _http, 'Room', title);
+  }
 }
--- src/app/views/pages/rooms/room/room-list/room-list.component.ts
+++ src/app/views/pages/rooms/room/room-list/room-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ShareService } from 'src/app/core/services/general/share.service';
+import { RoomService } from 'src/app/core/services/room/room.service';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
-  styleUrls: ['./room-list.component.scss']
+  styleUrls: ['./room-list.component.scss'],
 })
 export class RoomListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  dataSource: any;
+  dataSourceStatus: any = [
+    {
+      id: 1,
+      name: 'Hoạt động',
+    },
+    {
+      id: 2,
+      name: 'Ngưng hoạt động',
+    },
+  ];
+  @ViewChild('modalChild', { static: false }) modalChild;
+  constructor(
+    private readonly _roomService: RoomService,
+    private readonly _shareService: ShareService
+  ) {
+    this.showModal = this.showModal.bind(this);
+    this.fnDelete = this.fnDelete.bind(this);
   }
 
+  ngOnInit() {
+    this.dataSource = this._roomService.loadDataGrid();
+  }
+
+  reloadGrid() {
+    this.dataSource.reload();
+  }
+
+  fnDelete(e) {
+    let id = e.row.data.id;
+    this._shareService.deleteRowGrid(id,this._roomService,()=>{
+      this.dataSource.reload();
+    })
+  }
+
+  showModal(e) {
+    this.modalChild.showChildModal(e.row.data);
+  }
 }
